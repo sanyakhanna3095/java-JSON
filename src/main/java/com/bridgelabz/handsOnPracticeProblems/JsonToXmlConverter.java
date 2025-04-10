729ce939b6e5b060dba923842d9495b33581965d
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-04-10 15:42:05 +0530

    Convert JSON to XML format.

--- src/main/java/com/bridgelabz/handsOnPracticeProblems/JsonToXmlConverter.java
+++ src/main/java/com/bridgelabz/handsOnPracticeProblems/JsonToXmlConverter.java
@@ -1,4 +1,39 @@
 package com.bridgelabz.handsOnPracticeProblems;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class JsonToXmlConverter {
+    public static void main(String[] args) {
+
+        try {
+            String s = new String(Files.readAllBytes(Paths.get("/Users/sanyakhanna/Desktop/bridgelabz-workspace/java-JSON/src/main/java/com/bridgelabz/practiceProblems/studentInfo.json"))).trim();
+            String xml="";
+            if(s.startsWith("{")){
+                JSONObject obj=new JSONObject(s);
+                xml = XML.toString(obj);
+            }
+            else if (s.startsWith("[")) {
+                JSONArray arr = new JSONArray(s);
+                xml=XML.toString(arr);
+            }
+            else{
+                System.out.println("Error");
+            }
+            System.out.println("Converted XML:\n" + xml);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
+/*
+O/P->
+<subjects>Math</subjects><subjects>Science</subjects><name>Sanya Khanna</name><id>101</id><email>sanya.khanna@example.com</email><age>21</age>
+ */
