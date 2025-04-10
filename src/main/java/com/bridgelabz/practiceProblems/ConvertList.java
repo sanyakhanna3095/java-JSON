1b4a8e186f8e1bee566e1ef55f8474078b8de640
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-04-10 13:56:37 +0530

    Convert a list of Java objects into a JSON array

--- src/main/java/com/bridgelabz/practiceProblems/ConvertList.java
+++ src/main/java/com/bridgelabz/practiceProblems/ConvertList.java
@@ -22,13 +22,8 @@ public class ConvertList {
 
         // Getters and Setters
         public String getName() { return name; }
-        public void setName(String name) { this.name = name; }
-
         public int getAge() { return age; }
-        public void setAge(int age) { this.age = age; }
-
         public String getEmail() { return email; }
-        public void setEmail(String email) { this.email = email; }
 
 
     }
