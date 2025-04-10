817b9f4dc6bcebc0608727f3455a5e9214f95ae7
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-04-10 14:00:10 +0530

    Convert a list of Java objects into a JSON array

--- src/main/java/com/bridgelabz/practiceProblems/ConvertList.java
+++ src/main/java/com/bridgelabz/practiceProblems/ConvertList.java
@@ -28,18 +28,46 @@ public class ConvertList {
 
     }
 
-    public static void main(String[] args) throws JsonProcessingException {
-        List<Student> l=new ArrayList<>();
-        l.add(new Student("Sanya Khanna", 21, "sanya12@gmail.com"));
-        l.add(new Student("Sehajpreet Kaur", 22, "sehaj08765@gmail.com"));
-        l.add(new Student("Aarush Sharma", 16, "aaru110@gmail.com"));
-        l.add(new Student("Prachi Jain", 23, "prachi0987@gmail.com"));
+    public static void main(String[] args){
+        try {
+            List<Student> l = new ArrayList<>();
+            l.add(new Student("Sanya Khanna", 21, "sanya12@gmail.com"));
+            l.add(new Student("Sehajpreet Kaur", 22, "sehaj08765@gmail.com"));
+            l.add(new Student("Aarush Sharma", 16, "aaru110@gmail.com"));
+            l.add(new Student("Prachi Jain", 23, "prachi0987@gmail.com"));
 
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonArray = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(l);
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonArray = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(l);
 
-        // Print the JSON array
-        System.out.println("JSON Array:");
-        System.out.println(jsonArray);
+            System.out.println("JSON Array:");
+            System.out.println(jsonArray);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
+/*
+O/P->
+JSON Array:
+[ {
+  "name" : "Sanya Khanna",
+  "age" : 21,
+  "email" : "sanya12@gmail.com"
+}, {
+  "name" : "Sehajpreet Kaur",
+  "age" : 22,
+  "email" : "sehaj08765@gmail.com"
+}, {
+  "name" : "Aarush Sharma",
+  "age" : 16,
+  "email" : "aaru110@gmail.com"
+}, {
+  "name" : "Prachi Jain",
+  "age" : 23,
+  "email" : "prachi0987@gmail.com"
+} ]
+
+ */
